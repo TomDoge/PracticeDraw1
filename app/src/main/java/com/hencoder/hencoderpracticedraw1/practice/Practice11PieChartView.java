691e3486e9c423cccd28a433242d560ea28480fe
penglei <yupengniaosanshi@gmail.com>
2018-02-28 00:01:40 +0800

    完成饼图，注释没有画

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
@@ -2,11 +2,34 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import java.util.Arrays;
+
 public class Practice11PieChartView extends View {
+    private Paint mPaint = new Paint();
+
+    private static String[] sTextArray = {"Froyo", "GB", "ICS", "JB", "Kitkat", "L", "M"};
+    private static int[] sColorArray = {Color.BLACK, Color.MAGENTA,
+            Color.GRAY, Color.GREEN, Color.BLUE, Color.RED, Color.YELLOW};
+    private static double[] sDegreeArray;
+    private static double sDegreeDiv = 2;
+    private static RectF sRectF = new RectF(250, 100, 750, 600);
+
+    static {
+        int[] heightArray = {1, 10, 15, 170, 300, 350, 150};
+        final double sum = Arrays.stream(heightArray).sum();
+        double remainDegree = 360 - sDegreeDiv * (heightArray.length - 1);
+        sDegreeArray = Arrays.stream(heightArray)
+                .mapToDouble(x -> x / sum)
+                .map(x -> x * remainDegree)
+                .toArray();
+    }
 
     public Practice11PieChartView(Context context) {
         super(context);
@@ -26,5 +49,15 @@ public class Practice11PieChartView extends View {
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画饼图
+        mPaint.setAntiAlias(true);
+        int offset = 20;
+        double startAngle = 0;
+        for (int i = 0; i < sTextArray.length; i++) {
+            mPaint.setColor(sColorArray[i]);
+            if (i == 5) canvas.translate(-offset, -offset);
+            canvas.drawArc(sRectF, (float) startAngle, (float) sDegreeArray[i], true, mPaint);
+            if (i == 5) canvas.translate(offset, offset);
+            startAngle = startAngle + sDegreeArray[i] + sDegreeDiv;
+        }
     }
 }
--- app/src/test/java/com/hencoder/hencoderpracticedraw1/ExampleUnitTest.java
+++ app/src/test/java/com/hencoder/hencoderpracticedraw1/ExampleUnitTest.java
@@ -2,6 +2,8 @@ package com.hencoder.hencoderpracticedraw1;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 /**
@@ -13,5 +15,19 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void test() {
+        int[] heightArray = {1, 10, 15, 170, 300, 350, 150};
+        float sum = 0;
+        for (int i : heightArray) {
+            sum += i;
+        }
+        float[] degreeArray = new float[heightArray.length];
+        for (int i = 0; i < degreeArray.length; i++) {
+            degreeArray[i] = heightArray[i] / sum * 100;
+        }
+        System.out.println(Arrays.toString(degreeArray));
     }
 }
